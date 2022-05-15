7e917e702faeebed24084e996fb06fff24ceeb37
zephyr06 <sen.zephyr.wang@gmail.com>
2022-05-15 10:43:41 -0400

    update EnergyPerf settings

--- CompareWithBaseline/EnergyPerformance/CompareEnergyLL.sh
+++ CompareWithBaseline/EnergyPerformance/CompareEnergyLL.sh
@@ -42,8 +42,8 @@ do
 	python $ROOT_PATH/CompareWithBaseline/edit_yaml.py --entry "elimIte" --value 1000
 	perform_optimization
 	
-	# dog-leg, not eliminated, exact Jacobian
-	python $ROOT_PATH/CompareWithBaseline/edit_yaml.py --entry "optimizerType" --value 1
+	# LM, not eliminated, exact Jacobian
+	python $ROOT_PATH/CompareWithBaseline/edit_yaml.py --entry "optimizerType" --value 2
 	python $ROOT_PATH/CompareWithBaseline/edit_yaml.py --entry "exactJacobian" --value 1
 	python $ROOT_PATH/CompareWithBaseline/edit_yaml.py --entry "elimIte" --value 0
 	perform_optimization
