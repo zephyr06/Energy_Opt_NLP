35e50871023b58010676ecec7f9779a00b3791e9
zephyr06 <sen.zephyr.wang@gmail.com>
2023-02-22 16:16:05 -0500

    remove commented scripts

--- CompareWithBaseline/ControlPerformance/CompareControlLL.sh
+++ CompareWithBaseline/ControlPerformance/CompareControlLL.sh
@@ -58,5 +58,3 @@ python $ROOT_PATH/CompareWithBaseline/$title/Visualize_performance.py  --minTask
 python $ROOT_PATH/CompareWithBaseline/$title/Visualize_performance.py  --minTaskNumber 5 --title $title  --maxTaskNumber $MaxTaskNumber --data_source "Time"
 python $ROOT_PATH/CompareWithBaseline/$title/Visualize_performance.py  --minTaskNumber 5 --title $title  --maxTaskNumber $MaxTaskNumber --data_source "RTA"
 python $ROOT_PATH/CompareWithBaseline/$title/draw_box_plot.py
-# python $ROOT_PATH/CompareWithBaseline/$title/Visualize_performance.py  --minTaskNumber 5 --title $title  --maxTaskNumber $MaxTaskNumber --data_source "Time"
-# python $ROOT_PATH/CompareWithBaseline/$title/Visualize_performance.py  --minTaskNumber 5 --title $title  --maxTaskNumber $MaxTaskNumber --data_source "RTA"
