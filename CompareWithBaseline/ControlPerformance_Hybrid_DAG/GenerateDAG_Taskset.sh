f147b8083b9065fe4e3435dca13128a64982b600
zephyr06 <sen.zephyr.wang@gmail.com>
2023-08-24 16:29:22 -0400

    add GenerateTaskSet

---
+++ CompareWithBaseline/ControlPerformance_Hybrid_DAG/GenerateDAG_Taskset.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# ************** Adjust settings there **************
+ROOT_PATH="/home/zephyr/Programming/Energy_Opt_NLP"
+title="ControlPerformance_Hybrid_DAG"
+MaxTaskNumber=3
+# ***************************************************
+
+cp parameters.yaml $ROOT_PATH/sources/parameters.yaml
+# clear buffer file content
+cd $ROOT_PATH/release
+make GenerateTaskSet -j2
+
+
+for (( jobNumber=3; jobNumber<=$MaxTaskNumber; jobNumber++ ))
+do
+	echo "$title iteration is: $jobNumber"	
+	./tests/GenerateTaskSet --taskType 3 --N $jobNumber --taskSetNumber 100 --NumberOfProcessor 2 --directory $ROOT_PATH/TaskData/$title/	
+done
