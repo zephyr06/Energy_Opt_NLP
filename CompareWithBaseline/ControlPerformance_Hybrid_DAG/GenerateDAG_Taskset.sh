5b703a9807f12b1e8d21da9e2ac52b80c3404930
zephyr06 <sen.zephyr.wang@gmail.com>
2023-08-24 16:54:44 -0400

    fix path issue

--- CompareWithBaseline/ControlPerformance_Hybrid_DAG/GenerateDAG_Taskset.sh
+++ CompareWithBaseline/ControlPerformance_Hybrid_DAG/GenerateDAG_Taskset.sh
@@ -9,11 +9,12 @@ MaxTaskNumber=3
 cp parameters.yaml $ROOT_PATH/sources/parameters.yaml
 # clear buffer file content
 cd $ROOT_PATH/release
+cmake ..
 make GenerateTaskSet -j2
 
 
 for (( jobNumber=3; jobNumber<=$MaxTaskNumber; jobNumber++ ))
 do
 	echo "$title iteration is: $jobNumber"	
-	./tests/GenerateTaskSet --taskType 3 --N $jobNumber --taskSetNumber 100 --NumberOfProcessor 2 --directory $ROOT_PATH/TaskData/$title/	
+	./tests/GenerateTaskSet --taskType 3 --N $jobNumber --taskSetNumber 100 --NumberOfProcessor 2 --directory $ROOT_PATH/TaskData/$title	
 done
