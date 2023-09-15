afa9eb4b2e833259f96835f717bb50679751ac4e
zephyr06 <sen.zephyr.wang@gmail.com>
2023-09-14 22:24:00 -0400

    update batch test

--- tests/testPerformanceBatch.cpp
+++ tests/testPerformanceBatch.cpp
@@ -6,10 +6,10 @@ TEST(overall, batch) {
         "/home/zephyr/Programming/Energy_Opt_NLP/TaskData/"
         "ControlPerformance_Hybrid_DAG/N0/";
     ClearResultFiles(dataset_path);
-    Obj_Pow = 1;
+    Obj_Pow = 2;
     enableReorder = 1;
     double obj_act = BatchOptimizeNasri19(0);
-    EXPECT(obj_act <= 0.4125);
+    EXPECT(obj_act <= 0.67);
 }
 
 int main() {
